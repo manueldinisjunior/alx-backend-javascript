044e03d3720983ed0f7226c1577ba38de6e88ed5
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-09-26 12:10:07 +0200

    All

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-console */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const x = uploadPhoto();
-  const y = createUser();
-  return Promise.all([x, y]).then(
-    (data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
-    () => console.log('Signup system offline'),
-  );
+  const photo = uploadPhoto();
+  const user = createUser();
+  return Promise.all([photo, user])
+    .then((response) => {
+      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
+    })
+    .catch(() => { console.log('Signup system offline'); });
 }
